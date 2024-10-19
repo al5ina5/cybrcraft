f2d506fbb2a69b16b3b1919007a2714c87502e4c
Sebastian Alsina <alsinas@me.com>
2024-10-19 05:48:08 -0400

    Font Fixeds

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,14 +8,12 @@ export default function Home() {
 
       <div className="bg-gray-300 emboss p-1 relative z-20 max-w-md w-full">
         <div className="p-6 font-mono space-y-4  bg-white deboss">
-          <div className="p-2">
-            <p className="text-4xl font-extrabold">CYBRCRAFT</p>
-            <p className="text-2xl">A developing Creative Minecraft community.</p>
-          </div>
-          <div className="text-2xl bg-black text-white text-center p-2">
+          <p className="text-2xl font-extrabold">CYBRCRAFT</p>
+          <p className="text-lg">A developing Creative Minecraft community.</p>
+          <div className="text-lg bg-black text-white text-center p-2">
             <p>IP: play.cybrcraft.xyz</p>
           </div>
-          <div className="text-2xl flex flex-col">
+          <div className="text-lg flex flex-col">
             <a className="bllock underline hover:no-underline" href="https://map.cybrcraft.xyz/" target="_blank">{'>'} Open Map</a>
             <a className="underline hover:no-underline" href="/discord" target="_blank">{'>'} Join Discord</a>
           </div>
